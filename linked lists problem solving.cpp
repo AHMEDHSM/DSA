a16c39b53c8079e517dc7c006622aca5390c0133
AHMEDHSM <148036234+AHMEDHSM@users.noreply.github.com>
2025-05-08 14:06:45 +0300

    Add files via upload

---
+++ linked lists problem solving.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    void print(ListNode* head)
+    {
+        ListNode* cur = head;
+        while (cur)
+        {
+            std::cout << cur->val;
+            cur = cur->next;
+        }
+    }
+    void reorderList(ListNode* head) {
+        ListNode* slow = head, * fast = head->next;
+        //reach mid of list
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        //reverse the second half
+        ListNode* NewHead = slow->next;
+        ListNode* left = nullptr, * right = NewHead;
+        while (right)
+        {
+            ListNode* temp = right->next;
+            right->next = left;
+            left = right;
+            right = temp;
+            
+        }
+       // disconnect the list
+        slow->next = nullptr;
+
+        //merge two lists
+
+        ListNode* first = head, * second = left;
+        while (first && second)
+        {   
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
+            first->next = second;
+            second->next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+
+    }
+};
+int main() {
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(3);
+    ListNode* node4 = new ListNode(4);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    Solution s1;
+    s1.print(node1);
+    s1.reorderList(node1);
+    s1.print(node1);
+}
